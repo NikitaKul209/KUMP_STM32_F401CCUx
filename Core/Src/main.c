5f369c2abe6fb60d2df284a762b70df7490c5b13
NikitaKul209 <Nikita2099n@mail.ru>
2023-11-03 12:47:11 +0300

    fix

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -53,36 +53,39 @@
 /* USER CODE BEGIN PV */
 
 const unsigned char I2C_DEV_ADDR = 0x44;
-
-float adc;
+const unsigned char DEV_ADDR = 0x40;
 
 bool rx_done_flag = false;
 bool tx_done_flag = false;
 bool tx_ready_flag = false;
-uint32_t uart_error;
 
-
-
-int ii_max = 256;
-int  fl_err_fragment_char_min = 0;
-
-
-
-
-int receive_byte = 0;
-uint32_t error_code = 0;
 bool FE_Error = false;
 bool OE_Error = false;
 bool PE_Error = false;
-uint8_t uart_inbuf[256] = {0};
-uint8_t uart_outbuf[256]= {0};
+
+float adc;
+
+int ii_max = 256;
+int fl_err_fragment_char_min = 0;
+int receive_byte = 0;
+
+uint32_t error_code = 0;
+
+uint8_t uart_inbuf[256] = { 0 };
+uint8_t uart_outbuf[256] = { 0 };
 uint8_t *p_uart_inbuf = uart_inbuf;
 
-union unn_t
-{
-  char ch_val[2];
-  unsigned short w_val;
+union unn_t {
+	char ch_val[2];
+	unsigned short w_val;
 } unn;
+
+enum states {
+	start_uart_receive_data,
+	start_uart_transmit_data,
+	check_received_data,
+	modbus_functions
+};
 
 /* USER CODE END PV */
 
@@ -99,84 +102,72 @@ signed char Check_Uart_inbuff(void);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
-  /* USER CODE BEGIN 1 */
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
+	/* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+	/* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+	/* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+	/* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+	/* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+	/* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_ADC1_Init();
-  MX_USART1_UART_Init();
-  MX_I2C1_Init();
-  MX_TIM2_Init();
-  MX_TIM3_Init();
-  /* USER CODE BEGIN 2 */
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_ADC1_Init();
+	MX_USART1_UART_Init();
+	MX_I2C1_Init();
+	MX_TIM2_Init();
+	MX_TIM3_Init();
+	/* USER CODE BEGIN 2 */
 
-  TIM_GET_CLEAR_IT(&htim2,TIM_IT_UPDATE);
-  TIM_GET_CLEAR_IT(&htim3,TIM_IT_UPDATE);
+	TIM_GET_CLEAR_IT(&htim2,TIM_IT_UPDATE);
+	TIM_GET_CLEAR_IT(&htim3,TIM_IT_UPDATE);
 
-//  HAL_UART_Receive_IT (&huart1, p_uart_inbuf++, 1);
+	int state = start_uart_receive_data;
+	/* USER CODE END 2 */
 
-enum states{
-	start_uart_receive_data,
-	start_uart_transmit_data,
-	check_received_data,
-	modbus_functions
-};
-int state = start_uart_receive_data;
-  /* USER CODE END 2 */
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
+	while (1) {
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-
-	switch(state){
+		switch (state) {
 
 		case start_uart_receive_data:
 
-			  HAL_UART_Receive_IT (&huart1, p_uart_inbuf++, 1);
-			  state = check_received_data;
+			HAL_UART_Receive_IT(&huart1, p_uart_inbuf++, 1);
+			state = check_received_data;
 
 			break;
 
 		case check_received_data:
 
-			if (rx_done_flag){
+			if (rx_done_flag) {
 				rx_done_flag = false;
+				p_uart_inbuf = uart_inbuf;
 
-				if (Check_Uart_inbuff()== 0){
-
+				if ((Check_Uart_inbuff() == 0) && (uart_inbuf[0] == DEV_ADDR)) {
 					state = modbus_functions;
-					receive_byte = 0;
-				}
-				else{
-					p_uart_inbuf = 0;
-					receive_byte = 0;
-					HAL_UART_Transmit_IT(&huart1, (uint8_t*) "ERROR", 0x6);
+				} else {
+
+					HAL_TIM_Base_Stop_IT(&htim3);
 					state = start_uart_receive_data;
 				}
+				receive_byte = 0;
 
 			}
 
@@ -184,62 +175,20 @@ int state = start_uart_receive_data;
 
 		case modbus_functions:
 
-			 p_uart_inbuf = 0;
-			 state = start_uart_transmit_data;
+			state = start_uart_transmit_data;
 			break;
 
 		case start_uart_transmit_data:
 
-			 if(tx_ready_flag == true){
-				 tx_ready_flag = false;
-				 HAL_UART_Transmit_IT(&huart1, (uint8_t*) "OK", 0x3);
-				 state = start_uart_receive_data;
+			if (tx_ready_flag == true) {
+				tx_ready_flag = false;
+				HAL_UART_Transmit_IT(&huart1, (uint8_t*) "OK", 0x3);
+				state = start_uart_receive_data;
 
-			 }
-			 break;
+			}
+			break;
 
-
-	}
-//
-//
-//	  if(rx_done_flag ==true){
-//
-//		  p_uart_inbuf = uart_inbuf;
-//
-//
-//		  if(FE_Error|OE_Error|PE_Error){
-//			  	  FE_Error = false;
-//			  	  PE_Error = false;
-//			  	  OE_Error = false;
-//			 	 HAL_UART_Receive_IT (&huart1, p_uart_inbuf++, 1);
-//		  }
-//		  else{
-//
-//
-//
-//			  if(crc16in(receive_byte, &unn, uart_inbuf)!=0);
-//
-//			  HAL_UART_Receive_IT (&huart1, p_uart_inbuf++, 1);
-//
-//		  }
-//
-//
-//		  rx_done_flag = false;
-//
-//	  }
-//
-//
-//
-//
-//	 if(tx_ready_flag == true){
-//		 tx_ready_flag = false;
-//		 HAL_UART_Transmit_IT(&huart1, uart_inbuf, receive_byte);
-//		 receive_byte = 0;
-//	  }
-
-
-
-
+		}
 
 
 //	  HAL_ADC_Start(&hadc1);
@@ -248,83 +197,75 @@ int state = start_uart_receive_data;
 // 	  		  HAL_ADC_Stop(&hadc1);
 //	  	  };
 
-
 //	  HAL_Delay(100);
 //	  HAL_SPI_Transmit(&hspi1, spi_buff, 0x1, 1000);
 //	  HAL_Delay(1000);
-
 
 //	  HAL_I2C_Master_Transmit(&hi2c1, (SPI_DEV_ADDR<<1), SPI_REG_ADDR, 2,1000);
 //	  HAL_I2C_Master_Receive(&hi2c1, SPI_DEV_ADDR<<1, spi_read_buff, 0x4,100);
 //	  HAL_Delay(100);
 
-  }
+	}
 
-    /* USER CODE END WHILE */
+	/* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
+	/* USER CODE BEGIN 3 */
 
-
-  /* USER CODE END 3 */
+	/* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+	/** Configure the main internal regulator output voltage
+	 */
+	__HAL_RCC_PWR_CLK_ENABLE();
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		Error_Handler();
+	}
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSE;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSE;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
+		Error_Handler();
+	}
 }
 
 /* USER CODE BEGIN 4 */
 
-signed char Check_Uart_inbuff(void){
+signed char Check_Uart_inbuff(void) {
 
-
-
-	if (receive_byte >= ii_max){
+	if (receive_byte >= ii_max) {
 
 		return -4;
 	}
 
-	if (receive_byte < 5){
+	if (receive_byte < 5) {
 
 		return -3;
 	}
 
-	if(FE_Error|OE_Error|PE_Error){
+	if (FE_Error || OE_Error || PE_Error) {
 
 		FE_Error = 0;
 		OE_Error = 0;
@@ -333,7 +274,7 @@ signed char Check_Uart_inbuff(void){
 		return -2;
 	}
 
-	if(crc16in(receive_byte, &unn, uart_inbuf)!=0){
+	if (crc16in(receive_byte, &unn, uart_inbuf) != 0) {
 
 		return -1;
 	}
@@ -341,124 +282,107 @@ signed char Check_Uart_inbuff(void){
 	return 0;
 }
 
+char crc16in(unsigned char size, union unn_t *unn, unsigned char *inbuf) {
+	unsigned short w = 0xffff, w1;
+	char shift_cnt, jj;
+	unsigned short ii1 = 0;
 
-char crc16in(unsigned char size, union unn_t *unn, unsigned char *inbuf)
-{
-  unsigned short w = 0xffff, w1;
-  char shift_cnt, jj;
-  unsigned short ii1 = 0;
+	union {
+		char c[2];
+		unsigned short w;
+	} u;
 
-  union
-  {
-    char c[2];
-    unsigned short w;
-  } u;
+	size = size - 2;
+	u.c[0] = inbuf[size];
+	u.c[1] = inbuf[size + 1];
+	jj = size;
 
-  size = size - 2;
-  u.c[0] = inbuf[size];
-  u.c[1] = inbuf[size + 1];
-  jj = size;
+	for (; jj > 0; jj--) {
+		w1 = (w >> 8) << 8;
+		w = (w1 + ((w - w1) ^ (unsigned short) inbuf[ii1++]));
 
-  for (; jj > 0; jj--)
-    {
-      w1 = (w >> 8) << 8;
-      w = (w1 + ((w - w1) ^ (unsigned short)inbuf[ii1++]));
+		for (shift_cnt = 0; shift_cnt < 8; shift_cnt++) {
+			if ((w & 0x01) == 1)
+				w = ((w >> 1) ^ 0xa001);
+			else
+				w >>= 1;
+		}
+	}
+	unn->w_val = w;
 
-      for (shift_cnt = 0; shift_cnt < 8; shift_cnt++)
-        {
-          if ((w & 0x01) == 1)
-            w = ((w >> 1) ^ 0xa001);
-          else
-            w >>= 1;
-        }
-    }
-  unn->w_val = w;
-
-  if (w == u.w)
-    return 0;
-  else
-    return (char) -1;
+	if (w == u.w)
+		return 0;
+	else
+		return (char) -1;
 }
 
-
-
-
-void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart){
-	  if(huart == &huart1){
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart) {
+	if (huart == &huart1) {
 		tx_done_flag = true;
 
-	  }
+	}
 }
 
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
 
-void HAL_UART_RxCpltCallback (UART_HandleTypeDef * huart){
+	if (huart == &huart1) {
 
-	  if(huart == &huart1) {
+		HAL_TIM_Base_Stop_IT(&htim2);
+		HAL_TIM_Base_Stop_IT(&htim3);
 
-		  error_code = HAL_UART_GetError(&huart1) ;
+		__HAL_TIM_SetCounter(&htim2,0x0);
+		__HAL_TIM_SetCounter(&htim3,0x0);
 
-		  HAL_TIM_Base_Stop_IT(&htim2);
-		  HAL_TIM_Base_Stop_IT(&htim3);
+		HAL_TIM_Base_Start_IT(&htim2);
+		HAL_TIM_Base_Start_IT(&htim3);
+		HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_12);
 
-		  __HAL_TIM_SetCounter(&htim2,0x0);
-		  __HAL_TIM_SetCounter(&htim3,0x0);
+		error_code = HAL_UART_GetError(&huart1);
+		HAL_UART_Receive_IT(&huart1, p_uart_inbuf++, 1);
+		receive_byte++;
 
-		  HAL_TIM_Base_Start_IT(&htim2);
-		  HAL_TIM_Base_Start_IT(&htim3);
-
-		  HAL_UART_Receive_IT (&huart1, p_uart_inbuf++, 1);
-		  receive_byte++;
-
-
-
-	  }
+	}
 
 }
 
+void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart) {
 
-void HAL_UART_ErrorCallback (UART_HandleTypeDef * huart){
+	if (huart == &huart1) {
 
-	 if(huart == &huart1){
+		if (error_code & UART_FLAG_FE) {
 
+			FE_Error = true;
+//		 			 	 HAL_UART_Transmit_IT(&huart1, (uint8_t*) "FE", 0x3);
+		}
+		if (error_code & UART_FLAG_PE) {
 
-		 		 if(error_code & UART_FLAG_FE){
+			PE_Error = true;
+//		 			 	 HAL_UART_Transmit_IT(&huart1, (uint8_t*) "PE", 0x3);
+		}
+		if (error_code & UART_FLAG_ORE) {
 
-		 			 	 FE_Error = true;
-		 			 	 HAL_UART_Transmit_IT(&huart1, (uint8_t*) "FE", 0x3);
-		 		 }
-		 		 if(error_code & UART_FLAG_PE){
+			OE_Error = true;
+//		 			    HAL_UART_Transmit_IT(&huart1, (uint8_t*) "OE", 0x3);
 
-		 			 	 PE_Error = true;
-		 			 	 HAL_UART_Transmit_IT(&huart1, (uint8_t*) "PE", 0x3);
-		 		 }
-		 		 if(error_code & UART_FLAG_ORE ){
+		}
+		error_code = 0;
 
-		 			    OE_Error = true;
-		 			    HAL_UART_Transmit_IT(&huart1, (uint8_t*) "OE", 0x3);
-
-		 		 }
-		 		 error_code = 0;
-
-	 }
+	}
 }
-
-
 
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1) {
+	}
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
--- Core/Src/stm32f4xx_it.c
+++ Core/Src/stm32f4xx_it.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file    stm32f4xx_it.c
-  * @brief   Interrupt Service Routines.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file    stm32f4xx_it.c
+ * @brief   Interrupt Service Routines.
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
@@ -41,7 +41,7 @@
 
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN PV */
-extern int  timFlag = 0;
+
 
 /* USER CODE END PV */
 
@@ -69,131 +69,117 @@ extern UART_HandleTypeDef huart1;
 /*           Cortex-M4 Processor Interruption and Exception Handlers          */
 /******************************************************************************/
 /**
-  * @brief This function handles Non maskable interrupt.
-  */
-void NMI_Handler(void)
-{
-  /* USER CODE BEGIN NonMaskableInt_IRQn 0 */
+ * @brief This function handles Non maskable interrupt.
+ */
+void NMI_Handler(void) {
+	/* USER CODE BEGIN NonMaskableInt_IRQn 0 */
 
-  /* USER CODE END NonMaskableInt_IRQn 0 */
-  /* USER CODE BEGIN NonMaskableInt_IRQn 1 */
-  while (1)
-  {
-  }
-  /* USER CODE END NonMaskableInt_IRQn 1 */
+	/* USER CODE END NonMaskableInt_IRQn 0 */
+	/* USER CODE BEGIN NonMaskableInt_IRQn 1 */
+	while (1) {
+	}
+	/* USER CODE END NonMaskableInt_IRQn 1 */
 }
 
 /**
-  * @brief This function handles Hard fault interrupt.
-  */
-void HardFault_Handler(void)
-{
-  /* USER CODE BEGIN HardFault_IRQn 0 */
+ * @brief This function handles Hard fault interrupt.
+ */
+void HardFault_Handler(void) {
+	/* USER CODE BEGIN HardFault_IRQn 0 */
 
-  /* USER CODE END HardFault_IRQn 0 */
-  while (1)
-  {
-    /* USER CODE BEGIN W1_HardFault_IRQn 0 */
-    /* USER CODE END W1_HardFault_IRQn 0 */
-  }
+	/* USER CODE END HardFault_IRQn 0 */
+	while (1) {
+		/* USER CODE BEGIN W1_HardFault_IRQn 0 */
+		/* USER CODE END W1_HardFault_IRQn 0 */
+	}
 }
 
 /**
-  * @brief This function handles Memory management fault.
-  */
-void MemManage_Handler(void)
-{
-  /* USER CODE BEGIN MemoryManagement_IRQn 0 */
+ * @brief This function handles Memory management fault.
+ */
+void MemManage_Handler(void) {
+	/* USER CODE BEGIN MemoryManagement_IRQn 0 */
 
-  /* USER CODE END MemoryManagement_IRQn 0 */
-  while (1)
-  {
-    /* USER CODE BEGIN W1_MemoryManagement_IRQn 0 */
-    /* USER CODE END W1_MemoryManagement_IRQn 0 */
-  }
+	/* USER CODE END MemoryManagement_IRQn 0 */
+	while (1) {
+		/* USER CODE BEGIN W1_MemoryManagement_IRQn 0 */
+		/* USER CODE END W1_MemoryManagement_IRQn 0 */
+	}
 }
 
 /**
-  * @brief This function handles Pre-fetch fault, memory access fault.
-  */
-void BusFault_Handler(void)
-{
-  /* USER CODE BEGIN BusFault_IRQn 0 */
+ * @brief This function handles Pre-fetch fault, memory access fault.
+ */
+void BusFault_Handler(void) {
+	/* USER CODE BEGIN BusFault_IRQn 0 */
 
-  /* USER CODE END BusFault_IRQn 0 */
-  while (1)
-  {
-    /* USER CODE BEGIN W1_BusFault_IRQn 0 */
-    /* USER CODE END W1_BusFault_IRQn 0 */
-  }
+	/* USER CODE END BusFault_IRQn 0 */
+	while (1) {
+		/* USER CODE BEGIN W1_BusFault_IRQn 0 */
+		/* USER CODE END W1_BusFault_IRQn 0 */
+	}
 }
 
 /**
-  * @brief This function handles Undefined instruction or illegal state.
-  */
-void UsageFault_Handler(void)
-{
-  /* USER CODE BEGIN UsageFault_IRQn 0 */
+ * @brief This function handles Undefined instruction or illegal state.
+ */
+void UsageFault_Handler(void) {
+	/* USER CODE BEGIN UsageFault_IRQn 0 */
 
-  /* USER CODE END UsageFault_IRQn 0 */
-  while (1)
-  {
-    /* USER CODE BEGIN W1_UsageFault_IRQn 0 */
-    /* USER CODE END W1_UsageFault_IRQn 0 */
-  }
+	/* USER CODE END UsageFault_IRQn 0 */
+	while (1) {
+		/* USER CODE BEGIN W1_UsageFault_IRQn 0 */
+		/* USER CODE END W1_UsageFault_IRQn 0 */
+	}
 }
 
 /**
-  * @brief This function handles System service call via SWI instruction.
-  */
-void SVC_Handler(void)
-{
-  /* USER CODE BEGIN SVCall_IRQn 0 */
+ * @brief This function handles System service call via SWI instruction.
+ */
+void SVC_Handler(void) {
+	/* USER CODE BEGIN SVCall_IRQn 0 */
 
-  /* USER CODE END SVCall_IRQn 0 */
-  /* USER CODE BEGIN SVCall_IRQn 1 */
+	/* USER CODE END SVCall_IRQn 0 */
+	/* USER CODE BEGIN SVCall_IRQn 1 */
 
-  /* USER CODE END SVCall_IRQn 1 */
+	/* USER CODE END SVCall_IRQn 1 */
 }
 
 /**
-  * @brief This function handles Debug monitor.
-  */
-void DebugMon_Handler(void)
-{
-  /* USER CODE BEGIN DebugMonitor_IRQn 0 */
+ * @brief This function handles Debug monitor.
+ */
+void DebugMon_Handler(void) {
+	/* USER CODE BEGIN DebugMonitor_IRQn 0 */
 
-  /* USER CODE END DebugMonitor_IRQn 0 */
-  /* USER CODE BEGIN DebugMonitor_IRQn 1 */
+	/* USER CODE END DebugMonitor_IRQn 0 */
+	/* USER CODE BEGIN DebugMonitor_IRQn 1 */
 
-  /* USER CODE END DebugMonitor_IRQn 1 */
+	/* USER CODE END DebugMonitor_IRQn 1 */
 }
 
 /**
-  * @brief This function handles Pendable request for system service.
-  */
-void PendSV_Handler(void)
-{
-  /* USER CODE BEGIN PendSV_IRQn 0 */
+ * @brief This function handles Pendable request for system service.
+ */
+void PendSV_Handler(void) {
+	/* USER CODE BEGIN PendSV_IRQn 0 */
 
-  /* USER CODE END PendSV_IRQn 0 */
-  /* USER CODE BEGIN PendSV_IRQn 1 */
+	/* USER CODE END PendSV_IRQn 0 */
+	/* USER CODE BEGIN PendSV_IRQn 1 */
 
-  /* USER CODE END PendSV_IRQn 1 */
+	/* USER CODE END PendSV_IRQn 1 */
 }
 
 /**
-  * @brief This function handles System tick timer.
-  */
-void SysTick_Handler(void)
-{
-  /* USER CODE BEGIN SysTick_IRQn 0 */
+ * @brief This function handles System tick timer.
+ */
+void SysTick_Handler(void) {
+	/* USER CODE BEGIN SysTick_IRQn 0 */
 
-  /* USER CODE END SysTick_IRQn 0 */
-  HAL_IncTick();
-  /* USER CODE BEGIN SysTick_IRQn 1 */
+	/* USER CODE END SysTick_IRQn 0 */
+	HAL_IncTick();
+	/* USER CODE BEGIN SysTick_IRQn 1 */
 
-  /* USER CODE END SysTick_IRQn 1 */
+	/* USER CODE END SysTick_IRQn 1 */
 }
 
 /******************************************************************************/
@@ -204,111 +190,98 @@ void SysTick_Handler(void)
 /******************************************************************************/
 
 /**
-  * @brief This function handles ADC1 global interrupt.
-  */
-void ADC_IRQHandler(void)
-{
-  /* USER CODE BEGIN ADC_IRQn 0 */
+ * @brief This function handles ADC1 global interrupt.
+ */
+void ADC_IRQHandler(void) {
+	/* USER CODE BEGIN ADC_IRQn 0 */
 
-  /* USER CODE END ADC_IRQn 0 */
-  HAL_ADC_IRQHandler(&hadc1);
-  /* USER CODE BEGIN ADC_IRQn 1 */
+	/* USER CODE END ADC_IRQn 0 */
+	HAL_ADC_IRQHandler(&hadc1);
+	/* USER CODE BEGIN ADC_IRQn 1 */
 
-  /* USER CODE END ADC_IRQn 1 */
+	/* USER CODE END ADC_IRQn 1 */
 }
 
 /**
-  * @brief This function handles TIM2 global interrupt.
-  */
-void TIM2_IRQHandler(void)
-{
-  /* USER CODE BEGIN TIM2_IRQn 0 */
+ * @brief This function handles TIM2 global interrupt.
+ */
+void TIM2_IRQHandler(void) {
+	/* USER CODE BEGIN TIM2_IRQn 0 */
 
-	if (TIM_GET_ITSTATUS(&htim2,TIM_IT_UPDATE )){
-
-		  HAL_TIM_Base_Stop_IT(&htim2);
-		  TIM_GET_CLEAR_IT(&htim2,TIM_IT_UPDATE);
-		  __HAL_TIM_SetCounter(&htim2,0x0);
-		  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_12);
-		  HAL_UART_AbortReceive_IT(&huart1);
-		  rx_done_flag = 1;
-
-
+	if (TIM_GET_ITSTATUS(&htim2,TIM_IT_UPDATE )) {
+		HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_12);
+		HAL_TIM_Base_Stop_IT(&htim2);
+		TIM_GET_CLEAR_IT(&htim2,TIM_IT_UPDATE);
+		__HAL_TIM_SetCounter(&htim2,0x0);
+		HAL_UART_AbortReceive_IT(&huart1);
+		rx_done_flag = 1;
 
 	}
 
-  /* USER CODE END TIM2_IRQn 0 */
-  HAL_TIM_IRQHandler(&htim2);
-  /* USER CODE BEGIN TIM2_IRQn 1 */
+	/* USER CODE END TIM2_IRQn 0 */
+	HAL_TIM_IRQHandler(&htim2);
+	/* USER CODE BEGIN TIM2_IRQn 1 */
 //  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_12);
-
-  /* USER CODE END TIM2_IRQn 1 */
+	/* USER CODE END TIM2_IRQn 1 */
 }
 
 /**
-  * @brief This function handles TIM3 global interrupt.
-  */
-void TIM3_IRQHandler(void)
-{
-  /* USER CODE BEGIN TIM3_IRQn 0 */
-if (TIM_GET_ITSTATUS(&htim3,TIM_IT_UPDATE )){
+ * @brief This function handles TIM3 global interrupt.
+ */
+void TIM3_IRQHandler(void) {
+	/* USER CODE BEGIN TIM3_IRQn 0 */
+	if (TIM_GET_ITSTATUS(&htim3,TIM_IT_UPDATE )) {
+		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_15);
+		HAL_TIM_Base_Stop_IT(&htim3);
+		TIM_GET_CLEAR_IT(&htim3,TIM_IT_UPDATE);
+		__HAL_TIM_SetCounter(&htim3,0x0);
+		tx_ready_flag = true;
 
-	  HAL_TIM_Base_Stop_IT(&htim3);
-	  TIM_GET_CLEAR_IT(&htim3,TIM_IT_UPDATE);
-	  __HAL_TIM_SetCounter(&htim3,0x0);
-	  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_15);
-	  tx_ready_flag = 1;
+	}
+	/* USER CODE END TIM3_IRQn 0 */
+	HAL_TIM_IRQHandler(&htim3);
+	/* USER CODE BEGIN TIM3_IRQn 1 */
 
-
-
-}
-  /* USER CODE END TIM3_IRQn 0 */
-  HAL_TIM_IRQHandler(&htim3);
-  /* USER CODE BEGIN TIM3_IRQn 1 */
-
-  /* USER CODE END TIM3_IRQn 1 */
+	/* USER CODE END TIM3_IRQn 1 */
 }
 
 /**
-  * @brief This function handles I2C1 event interrupt.
-  */
-void I2C1_EV_IRQHandler(void)
-{
-  /* USER CODE BEGIN I2C1_EV_IRQn 0 */
+ * @brief This function handles I2C1 event interrupt.
+ */
+void I2C1_EV_IRQHandler(void) {
+	/* USER CODE BEGIN I2C1_EV_IRQn 0 */
 
-  /* USER CODE END I2C1_EV_IRQn 0 */
-  HAL_I2C_EV_IRQHandler(&hi2c1);
-  /* USER CODE BEGIN I2C1_EV_IRQn 1 */
+	/* USER CODE END I2C1_EV_IRQn 0 */
+	HAL_I2C_EV_IRQHandler(&hi2c1);
+	/* USER CODE BEGIN I2C1_EV_IRQn 1 */
 
-  /* USER CODE END I2C1_EV_IRQn 1 */
+	/* USER CODE END I2C1_EV_IRQn 1 */
 }
 
 /**
-  * @brief This function handles I2C1 error interrupt.
-  */
-void I2C1_ER_IRQHandler(void)
-{
-  /* USER CODE BEGIN I2C1_ER_IRQn 0 */
+ * @brief This function handles I2C1 error interrupt.
+ */
+void I2C1_ER_IRQHandler(void) {
+	/* USER CODE BEGIN I2C1_ER_IRQn 0 */
 
-  /* USER CODE END I2C1_ER_IRQn 0 */
-  HAL_I2C_ER_IRQHandler(&hi2c1);
-  /* USER CODE BEGIN I2C1_ER_IRQn 1 */
+	/* USER CODE END I2C1_ER_IRQn 0 */
+	HAL_I2C_ER_IRQHandler(&hi2c1);
+	/* USER CODE BEGIN I2C1_ER_IRQn 1 */
 
-  /* USER CODE END I2C1_ER_IRQn 1 */
+	/* USER CODE END I2C1_ER_IRQn 1 */
 }
 
 /**
-  * @brief This function handles USART1 global interrupt.
-  */
-void USART1_IRQHandler(void)
-{
-  /* USER CODE BEGIN USART1_IRQn 0 */
+ * @brief This function handles USART1 global interrupt.
+ */
+void USART1_IRQHandler(void) {
+	/* USER CODE BEGIN USART1_IRQn 0 */
 
-  /* USER CODE END USART1_IRQn 0 */
-  HAL_UART_IRQHandler(&huart1);
-  /* USER CODE BEGIN USART1_IRQn 1 */
+	/* USER CODE END USART1_IRQn 0 */
+	HAL_UART_IRQHandler(&huart1);
+	/* USER CODE BEGIN USART1_IRQn 1 */
 
-  /* USER CODE END USART1_IRQn 1 */
+	/* USER CODE END USART1_IRQn 1 */
 }
 
 /* USER CODE BEGIN 1 */
